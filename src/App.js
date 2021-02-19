a08166f06c775597b4ef6dd5a1dff57d7fa15f29
spblewis <spblewis@gmail.com>
2021-02-18 23:14:47 -0600

    Added the drum keys

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
 import React from 'react';
 import './App.css';
 
+const drumkeys = [
+  {
+    text: 'Q'
+  },
+  {
+    text: 'W'
+  },
+  {
+    text: 'E'
+  },
+  {
+    text: 'A'
+  },
+  {
+    text: 'S'
+  },
+  {
+    text: 'D'
+  },
+  {
+    text: 'Z'
+  },
+  {
+    text: 'X'
+  },
+  {
+    text: 'C'
+  }
+]
+
+
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      drumset: drumkeys
+    };
   }
 
   render() {
-
+    const keys = this.state.drumset.map(x => 
+                  <div className="drum-pad">{x.text}</div>);
 
     return ( 
 
-      <div>
+      <div id="drum-machine">
 
         <h1>This'll be a drum set...</h1>
-
+        <div>{keys}</div>
       </div>
     )
   }
-}
+} 
